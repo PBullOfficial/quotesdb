0d92002e98262aebd552f61e6a96060077ee9505
PBullOfficial <pbulloch123@gmail.com>
2023-03-20 16:23:35 -0600

    completed database class for local testing

--- config/Database.php
+++ config/Database.php
@@ -1,40 +1,26 @@
 <?php 
     class Database {
         // DB Params
+        private $host = 'localhost';
+        private $port = '5432';
+        private $db_name = 'quotesdb';
+        private $username = 'postgres';
+        private $password = 'postgres';
         private $conn;
-        private $url;
-        private $dbparts;
-        private $hostname;
-        private $username;
-        private $password;
-        private $database;
-
-
-
-        function __construct(){
-            $this->conn = null;
-            $this->url = getenv('JAWSDB_URL');
-            $this->dbparts = parse_url($this->url);
-        }
 
         // DB Connect
         public function connect() {
-
-            $dbparts = parse_url($url);
-
-            $hostname = $this->dbparts['host'];
-            $username = $this->dbparts['user'];
-            $password = $this->dbparts['pass'];
-            $database = ltrim($this->dbparts['path'],'/');
+            $this->conn = null;
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
             try {
-                $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                $this->conn = new PDO($dsn, $this->username, $this->password);
+
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Success!";
             } catch(PDOException $e) {
+                // echo for tutorial, but log the error for production
                 echo "Connection Error: " . $e->getMessage();
             }
-
             return $this->conn;
         }
     } 
