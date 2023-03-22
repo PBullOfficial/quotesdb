60a91ae22a7a9ead40d917a0d7826af6822e8931
PBullOfficial <pbulloch123@gmail.com>
2023-03-22 16:29:49 -0600

    initial env variable commit

--- config/Database.php
+++ config/Database.php
@@ -1,28 +1,38 @@
 <?php 
     class Database {
         // DB Params
-        private $host = 'localhost';
-        private $port = '5432';
-        private $db_name = 'quotesdb';
-        private $username = 'postgres';
-        private $password = 'postgres';
         private $conn;
+        private $host;
+        private $port;
+        private $dbname;
+        private $username;
+        private $password;
+
+        public function __construct() {
+            $this->username = getenv('USERNAME');
+            $this->password = getenv('PASSWORD');
+            $this->dbname = getenv('DBNAME');
+            $this->host = getenv('HOST');
+            $this->port = getenv('PORT');
+        }
 
         // DB Connect
         public function connect() {
-            $this->conn = null;
-            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+            // instead of $this->conn = null;
+            if ($this->conn) {
+                //connection already exists, return it
+                return $this->conn;
+            } else {
 
-            try {
-                $this->conn = new PDO($dsn, $this->username, $this->password);
+                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch(PDOException $e) {
-                // echo for tutorial, but log the error for production
-                echo "Connection Error: " . $e->getMessage();
+                try {
+                    $this->conn = new PDO($dsn, $this->username, $this->password);
+                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return $this->conn;
+                } catch(PDOException $e) {
+                    echo "Connection Error: " . $e->getMessage();
+                }
             }
-            return $this->conn;
         }
     } 
-
-?>
