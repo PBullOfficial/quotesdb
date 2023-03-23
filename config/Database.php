38747d209d7c40abe7cd9ba3227abca2408e9409
PBullOfficial <pbulloch123@gmail.com>
2023-03-22 18:50:38 -0600

    redo database class

--- config/Database.php
+++ config/Database.php
@@ -1,38 +1,36 @@
 <?php 
-    class Database {
-        // DB Params
-        private $conn;
-        private $host;
-        private $port;
-        private $dbname;
-        private $username;
-        private $password;
+   class Database {
+     private $conn;
+     private $host;
+     private $port;
+     private $dbname;
+     private $username;
+     private $password;
+        
+     public function __construct() {
+         $this->username = getenv('USERNAME');
+         $this->password = getenv('PASSWORD');
+         $this->dbname = getenv('DBNAME');
+         $this->host = getenv('HOST');
+         $this->port = getenv('PORT');
+     }
 
-        public function __construct() {
-            $this->username = getenv('USERNAME');
-            $this->password = getenv('PASSWORD');
-            $this->dbname = getenv('DBNAME');
-            $this->host = getenv('HOST');
-            $this->port = getenv('PORT');
-        }
+     public function connect() {
+         // instead of $this->conn = null;
+         if ($this->conn) {
 
-        // DB Connect
-        public function connect() {
-            // instead of $this->conn = null;
-            if ($this->conn) {
-                //connection already exists, return it
-                return $this->conn;
-            } else {
+             return $this->conn; 
+         } else {        
 
-                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
-
-                try {
-                    $this->conn = new PDO($dsn, $this->username, $this->password);
-                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    return $this->conn;
-                } catch(PDOException $e) {
-                    echo "Connection Error: " . $e->getMessage();
-                }
-            }
-        }
-    } 
+             $dsn = "pgsql:host={$this->host};dbname={$this->dbname};";
+                
+       try {
+           $this->conn = new PDO($dsn, $this->username, $this->password);
+           $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              return $this->conn;
+          } catch(PDOException $e) {
+              echo 'Connection Error: ' . $e->getMessage();
+          }
+      }
+   }
+}
