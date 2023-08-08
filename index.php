47afc8cb974ba1b68400aa95502c16612a301be5
Peter Bulloch <pbulloch123@gmail.com>
2023-08-08 14:57:30 -0600

    completed quotesdb/index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header><h1>INF653 QuotedDB Midterm Project</h1></header>
+  </body>
+</html
